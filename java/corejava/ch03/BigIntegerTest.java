2df9adbcb8369f03f56bf0b35a914059eae8d556
Flyq <flyq951@gmail.com>
2016-07-25 10:02:23 +0800

    add ./java

---
+++ java/corejava/ch03/BigIntegerTest.java
@@ -0,0 +1,17 @@
+import java.math.*;
+import java.util.*;
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+public class BigInterger
+{
+    public static void main(String[] args)
+    {
+	Scanner = new Scanner(System.in);
+
+	System.out.print("How many number
