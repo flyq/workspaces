08264bea7a11a20fbdde38c2f8396857b8b41c71
Flyq <flyq951@gmail.com>
2016-09-11 00:32:03 +0800

    add .java in /ch94

---
+++ java/corejava/ch04/EmployeeTest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+/**
+ * This program tests the Employee class
+ * @version 1.11 2016-09-11
+ * @author Cay Horstman
+ */
+public class EmployeeTest
+{
+    public static void main(String[] args)
+	
